6b4a46a8f467a346e7a98a96d85f12a6e2f03001
Erica Goncalves <erica.goncalves@alumisoft.com.br>
2022-09-10 18:02:44 -0300

    Ajuste

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main';
+import Main from './pages/Main/index';
 
 export default function Routes() {
   return (
